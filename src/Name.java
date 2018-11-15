1554e7a4e60f311008ed960e70394b6d3259ee8c
Bailly <jeremy.bailly@etu.univ-lyon1.fr>
2018-11-15 11:25:24 +0100

    listCompar

--- src/Name.java
+++ src/Name.java
@@ -2,11 +2,39 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+//public class Name {
+//    private final String first, last;
+//
+//    public Name(String first, String last) {
+//        this.first = first;this.last = last;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Name name = (Name) o;
+//        return Objects.equals(first, name.first) &&
+//                Objects.equals(last, name.last);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(first, last);
+//    }
+//
+//    public static void main(String[] args) {
+//        Set<Name> s = new HashSet<Name>();
+//        s.add(new Name("Mickey", "Mouse"));
+//        System.out.println(s.contains(new Name("Mickey", "Mouse")));
+//    }
+//}
+
 public class Name {
     private final String first, last;
-
     public Name(String first, String last) {
-        this.first = first;this.last = last;
+        this.first = first;
+        this.last = last;
     }
 
     @Override
@@ -20,12 +48,12 @@ public class Name {
 
     @Override
     public int hashCode() {
-        return Objects.hash(first, last);
+        return 31 * first.hashCode() + last.hashCode();
     }
 
     public static void main(String[] args) {
         Set<Name> s = new HashSet<Name>();
-        s.add(new Name("Mickey", "Mouse"));
-        System.out.println(s.contains(new Name("Mickey", "Mouse")));
+        s.add(new Name("Donald", "Duck"));
+        System.out.println(s.contains(new Name("Donald", "Duck")));
     }
 }
